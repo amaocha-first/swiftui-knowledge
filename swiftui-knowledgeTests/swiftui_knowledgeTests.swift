addaf7b90c4c13d94b12377552da4d25d2ff3c2d
shota-nishizawa <guiltaugusthird03@gmail.com>
2020-09-01 10:57:46 +0900

    Initial Commit

---
+++ swiftui-knowledgeTests/swiftui_knowledgeTests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftui_knowledgeTests.swift
+//  swiftui-knowledgeTests
+//
+//  Created by shota-nishizawa on 2020/09/01.
+//  Copyright © 2020 cybozu. All rights reserved.
+//
+
+import XCTest
+@testable import swiftui_knowledge
+
+class swiftui_knowledgeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
